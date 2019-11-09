128175f7652ad31fdff2c77bb5ac5ea82c6cdb4f
Zachary Flower <zach@zacharyflower.com>
2019-11-08 17:14:27 -0700

    fix tracking js

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -1,7 +1,8 @@
 var idSite = 1;
-var piwikTrackingApiUrl = 'https://analytics.flower.codes/matomo.php';
+var matomoTrackingApiUrl = 'https://analytics.flower.codes/matomo.php';
 
-var _paq = _paq || [];
-
-_paq.push(['setTrackerUrl', piwikTrackingApiUrl]);
+var _paq = window._paq || [];
+_paq.push(['setTrackerUrl', matomoTrackingApiUrl]);
 _paq.push(['setSiteId', idSite]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);  
