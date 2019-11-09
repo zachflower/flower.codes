c14479cb16ea7d287a7455de6cd3ea5468ed1f03
Zachary Flower <zach@zacharyflower.com>
2019-11-08 17:10:48 -0700

    i have no idea what i am doing

---
+++ assets/js/tracking.js
@@ -0,0 +1,7 @@
+var idSite = 1;
+var piwikTrackingApiUrl = 'https://analytics.flower.codes/matomo.php';
+
+var _paq = _paq || [];
+
+_paq.push(['setTrackerUrl', piwikTrackingApiUrl]);
+_paq.push(['setSiteId', idSite]);
