4b27c3abf4aaf2a68c74ba99441b5422b53f6caf
Zachary Flower <zach@zacharyflower.com>
2019-12-05 11:47:15 -0700

    disable matomo (who needs analytics anyway?)

--- assets/js/tracking.js
+++
@@ -1,8 +0,0 @@
-var idSite = 1;
-var matomoTrackingApiUrl = 'https://analytics.flower.codes/matomo.php';
-
-var _paq = window._paq || [];
-_paq.push(['setTrackerUrl', matomoTrackingApiUrl]);
-_paq.push(['setSiteId', idSite]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);  
