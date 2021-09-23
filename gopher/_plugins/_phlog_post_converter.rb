26995f0256b5530965df6f2bb90d11d522111001
Zachary Flower <zach@flower.codes>
2021-09-23 10:33:06 -0600

    convert one of the posts to the new format, clean up phlog post converter logic

--- gopher/_plugins/_phlog_post_converter.rb
+++ gopher/_plugins/_phlog_post_converter.rb
@@ -15,21 +15,30 @@ module Jekyll
     end
 
     def convert(content)
-        citations = content.scan(/\[\[([^\]]*)\]\]/)
-        content.gsub!(/\[\[(.+)\]\]/i).with_index do |link, index|
-          "[#{index}]"
-        end
+      # capture links
+      citations = content.scan(/\[\[([^\]]*)\]\]/)
+      content.gsub!(/\[\[(.+)\]\]/i).with_index do |link, index|
+        "[#{index}]"
+      end
 
-        if citations.length > 0
-          content << "\n\n"
-          content << "---\n"
-          citations.each_with_index do |citation, index|
-            content << "[#{index}]: #{citation[0]}\n"
-          end
-          content << "---"
-        end
+      # wrap to 68 characters
+      content.wrap!(68)
 
-        content.wrap(68)
+      # trim trailing whitespace
+      content.chomp!
+
+      # append links as citations
+      if citations.length > 0
+        content << "\n\n"
+        content << "---\n"
+        citations.each_with_index do |citation, index|
+          content << "[#{index}]: #{citation[0]}\n"
+        end
+        content << "---"
+      end
+
+      # return it all
+      content
     end
   end
 end
