c4891b0862d3b7c2790d68bdd0c1b80eb4476d18
Zachary Flower <zach@flower.codes>
2022-12-07 07:36:24 -0700

    new post, new color scheme, new degrading method

--- bin/degrade.sh
+++ bin/degrade.sh
@@ -9,5 +9,5 @@ for f in *.jpg; do
 
     echo "Converting $f to gif"
 
-    convert "${f}" -verbose -format GIF -interlace GIF -resize 640\> -colorspace gray -colors 4 -ordered-dither h4x4a -set filename:f "%[t]-degraded" "%[filename:f].gif"
+    convert "${f}" -verbose -format GIF -interlace GIF -resize 640\> -colorspace gray -colors 4 -ordered-dither o8x8,2 -set filename:f "%[t]-degraded" "%[filename:f].gif"
 done
